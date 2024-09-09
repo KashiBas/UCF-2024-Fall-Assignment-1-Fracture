da18967bda9ef96c48f8c1e8b708d93ab7be9ea8
KashiBas <vi556810@ucf.edu>
2024-09-09 03:48:58 +0000

    Math Variable Cleanup

--- fracture.c
+++ fracture.c
@@ -43,9 +43,13 @@ printf("Point #1 entered: x1=",var,argcx", y1=",var,argcy)
 printf("Point #2 entered: x2=",var,argvx", y2=",var,argvy)
 // printf statements recall the values input by the user for each point
 
-dist=0
+dist = 0
+distx= 0
+disty= 0
 distradx = 0
 distrady = 0
+double distx = (argcx-argvx)
+double disty = (argcy-argvy)
 double distradx = pow(argcx-argvx,2)
 double distrady = pow(argcy-argvy,2)
 double dist = sqrt(distradx+distrady)
@@ -62,7 +66,7 @@ double calculatePerimeter() {
   double calculateDistance();
   // Recalling this function to print point values and previous calculations
 
-  perimeter = 2(abs(distradx)+abs(distrady))
+  perimeter = 2(abs(distx)+abs(disty))
 
   return(1.3)
 }
@@ -72,7 +76,7 @@ double calculateArea() {
   double calculateDistance();
   // Recalling this function to print point values and previous calculations
   area = 0
-  double area = (distradx*distrady)
+  double area = (distx*disty)
   printf("The area of the City is",%if,area)
   // Calculating the area of the city and saving it as a variable
 
@@ -85,8 +89,8 @@ double calculateWidth() {
   // Recalling this function to print point values and previous calculations
 
   width = 0
-  double width = abs(distradx)
-  printf("The Width of the City is",%if,distradx)
+  double width = abs(distx)
+  printf("The Width of the City is",%if,distx)
   // This Function uses the values previously calculated, makes it positive,
   // and saves the value of the total x distance as the city's width
   
@@ -99,8 +103,8 @@ double calculateHeight() {
   // Recalling this function to print point values and previous calculations
   
   height = 0
-  double height = abs(distrady)
-  printf("The Height of the City is",%if,distrady)
+  double height = abs(disty)
+  printf("The Height of the City is",%if,disty)
   // This Function uses the values previously calculated, makes it positive,
   // then saves the value of the total y distance as the city's height
   return(1.1)
